535e35f7cfc972ccec1b704eb4ff934e38229d93
Yaroslav19735 <yarkela1997@gmail.com>
2022-10-03 20:19:32 +0300

    3 task

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsMarkUp = ingredients.map((element) => {
+  const itemRef = document.createElement("li");
+  itemRef.textContent = element;
+  itemRef.classList.add("item");
+  return itemRef;
+});
+
+const listRef = document.querySelector("#ingredients");
+listRef.append(...ingredientsMarkUp);
