ddfd5145d0e94993221d8bbf8e594323da20bcc9
Yaroslav19735 <yarkela1997@gmail.com>
2022-10-05 13:47:03 +0300

    corrected

--- js/task-08.js
+++ js/task-08.js
@@ -1,18 +1,18 @@
 const formRef = document.querySelector(".login-form");
-const formData = {};
-
-formRef.addEventListener("submit", onFormSubmit);
-
+formRef.addEventListener("submit", onFormSubmit)
 function onFormSubmit(event) {
   event.preventDefault();
-  if (
-    !event.currentTarget.elements.email.value ||
-    !event.currentTarget.elements.password.value
-  ) {
-    alert("Все поля дожны быть заполены!");
+  const {
+    elements: { email, password }
+  } = event.currentTarget;
+  if (email.value === "" || password.value === "") {
+    return alert("Все поля дожны быть заполены!");
+  } else {
+    const view = {
+      email: email.value,
+      password: password.value
+    }
+    console.log(view);
+    event.currentTarget.reset();
   }
-  formData.email = event.currentTarget.elements.email.value;
-  formData.password = event.currentTarget.elements.password.value;
-  event.currentTarget.reset();
 }
-console.log(formData);
